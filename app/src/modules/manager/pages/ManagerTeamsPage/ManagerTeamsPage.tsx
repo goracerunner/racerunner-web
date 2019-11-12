ce57ff65a12a1aad1f53313e29771be23d1a57da
Ben Yap <bwyap@outlook.com>
2019-11-12 14:39:21 +1100

    feat: add WIP placeholders

--- app/src/modules/manager/pages/ManagerTeamsPage/ManagerTeamsPage.tsx
+++ app/src/modules/manager/pages/ManagerTeamsPage/ManagerTeamsPage.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+
 // import useStyles from "./styles";
 
 /**
  * TODO: add description
  */
 export const ManagerTeamsPage: React.FC = () => {
-  return null;
+  return (
+    <Container maxWidth="md" style={{ marginTop: "2rem" }}>
+      <Typography variant="button" color="error">
+        <b>This feature is currently unavailable.</b>
+      </Typography>
+    </Container>
+  );
 };
